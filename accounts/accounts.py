19b8a71d6b0d8004cff9d2150db7e128a6c6f7c8
Elafir <elafircruicac@gmail.com>
2023-03-27 14:25:08 -0400

    CRUD
    added the ability to update and delete user info.

--- accounts/accounts.py
+++ accounts/accounts.py
@@ -8,17 +8,30 @@ class Account:
     self.email = email
 
 
+# Procedures
 
 def get_id(reference):
   """ fetches the id of a user by their name or email """
 
-def authenticate(id, hash):
+def authenticate(id, salted_hash):
   """ checks if the provided password hash is the correct hash for the user with given id """
 
 def register(name, email, unsalted_hash):
   """ registers a user with given name and email; generates a salt and a salted hash for that user to save their password """
   id = secrets.randbits(64)
   final_hash = encrypt(unsalted_hash)
+
+def set_username(id, name):
+  """ updates the stored username for a given user """
+
+def set_email(id, email):
+  """ updates the stored email for a given user """
+
+def set_password(id, unsalted_hash):
+  """ updates the stored password for a given user """
+
+def remove_user(id):
+  """ removes an account from the database """
 
 def get_global_salt():
   """ standin """
