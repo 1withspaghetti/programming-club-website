3b49b7edf727c0f41f81adc17ed9c7734be80ae4
Tyler Place <tylerlplace@gmail.com>
2023-03-28 18:26:03 -0700

    Template routing - Added route for index.html - Fixed incorrect import in requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # @app.route("/route")
 # def function():
+
+@app.route("/")
+def index():
+    return render_template("index.html")
