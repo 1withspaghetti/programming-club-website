fae85c7a3bdb2c0c75c2c915a29aacd5f1f086f0
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-03-29 12:54:33 -0700

    basic routing

---
+++ auth/routing.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import url_for
+from os import listdir
+from os.path import isfile, join
+
+app = Flask(__name__)
+
+@app.route('/website/<long:post_id>')
+def show_post(post_id) -> str:
+    return f'Post {post_id}'
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return make_post()
+    else:
+        return make_get()
+
+def make_post():
+    'post request'
+
+def make_get():
+    'shows login screen'
+
+def get_and_index_static_file_urls() -> list:
+    files = [file for file in listdir('static') if isfile(join(url_for('static', file)))]
+    return files
