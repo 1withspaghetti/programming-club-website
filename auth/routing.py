b0a1af8094084d3bf1ee8341bc12463fcabe4fec
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-03-30 11:51:52 -0700

    removed dumb mistakes

--- auth/routing.py
+++ auth/routing.py
@@ -4,51 +4,48 @@ from flask import render_template
 from app import app
 from actions import actions
 
-class routing():
-
-    @app.route("/test")
-    def test_print():
+@app.route("/test")
+def test_print():
         return f'LHS Programming club website!'
 
-    @app.route("/login", methods = ['HEAD', 'POST'])
-    def login():
-        if request.method == 'POST':
-            if actions.valid_login(request.form['username'], request.form['password']):
-                actions.login_user(request.form['username'])
-            else:
-                error = 'invalid credentials'
-                render_template('invalid_credentials.html')
-        render_template('login.html', error)
+@app.route("/login", methods = ['HEAD', 'POST'])
+def login():
+    if request.method == 'POST':
+        if actions.valid_login(request.form['username'], request.form['password']):
+            actions.login_user(request.form['username'])
+        else:
+            error = 'invalid credentials'
+            render_template('invalid_credentials.html')
+    render_template('login.html', error)
         
 
-    @app.route("/refresh", methods = ['HEAD'])
-    def refresh():
-        pass
+@app.route("/refresh", methods = ['HEAD'])
+def refresh():
+    pass
 
-    @app.route("/logout", methods = ['PATCH'])
-    def logout():
-        pass
+@app.route("/logout", methods = ['PATCH'])
+def logout():
+    pass
 
-    @app.route("/register", methods = ['POST'])
-    def register():
-        pass
+@app.route("/register", methods = ['POST'])
+def register():
+    pass
 
-    @app.route("/update/username", methods = ['PATCH'])
-    def update_username():
-        pass
+@app.route("/update/username", methods = ['PATCH'])
+def update_username():
+    pass
 
-    @app.route("/update/email", methods = ['PATCH'])
-    def update_email():
-        pass
+@app.route("/update/email", methods = ['PATCH'])
+def update_email():
+    pass
 
-    @app.route("/update/password", methods = ['PATCH'])
-    def update_password():
-        pass
+@app.route("/update/password", methods = ['PATCH'])
+def update_password():
+    pass
 
-    @app.route("/delete", methods = ['DELETE'])
-    def delete():
-        pass
+@app.route("/delete", methods = ['DELETE'])
+def delete():
+    pass
 
-
-    def get_id(reference):
-        """ returns user id matching reference """
+def get_id(reference):
+    """ returns user id matching reference """
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from os.path import isfile, join
 from os import listdir
 from flask import Flask, url_for
-from auth.routing import routing
 from app import app
-
-
-router = routing()
 
 #generate absolute paths for static pages
 def get_and_index_static_file_urls() -> list:
