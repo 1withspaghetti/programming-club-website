6170e3dc70503dc8b7c010c84fb87cc0ab18b412
Elafir <elafircruicac@gmail.com>
2023-04-01 03:41:05 -0400

    Hacky Account Frontend
    Technically made the login and register account pages, but the css needs work, they aren't mobile compatible yet and they don't actually do anything.

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+@app.route("/register")
+def register():
+  return render_template("register.html")
